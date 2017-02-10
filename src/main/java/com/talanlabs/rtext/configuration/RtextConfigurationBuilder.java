962437d74b2cb8c5e7342e2ecadaad456338e24d
Gabriel Allaigre <gabriel.allaigre@talanlabs.com>
2017-02-10 17:05:20 +0100

    fix builder error

--- src/main/java/com/talanlabs/rtext/configuration/RtextConfigurationBuilder.java
+++ src/main/java/com/talanlabs/rtext/configuration/RtextConfigurationBuilder.java
@@ -4,6 +4,9 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.reflect.TypeToken;
 import com.talanlabs.rtext.IRtextTypeAdapter;
 import com.talanlabs.rtext.IRtextTypeAdapterFactory;
+import com.talanlabs.rtext.Rtext;
+import com.talanlabs.typeadapters.ITypeAdapter;
+import com.talanlabs.typeadapters.ITypeAdapterFactory;
 import com.talanlabs.typeadapters.TypeAdaptersHelper;
 
 import java.lang.reflect.Type;
@@ -13,50 +16,89 @@ import java.util.List;
 
 public class RtextConfigurationBuilder {
 
-    private RtextConfigurationImpl rtextConfiguration;
+	private RtextConfigurationImpl rtextConfiguration;
 
-    private RtextConfigurationBuilder() {
-        super();
+	private RtextConfigurationBuilder() {
+		super();
 
-        this.rtextConfiguration = new RtextConfigurationImpl();
-    }
+		this.rtextConfiguration = new RtextConfigurationImpl();
+	}
 
-    public static RtextConfigurationBuilder newBuilder() {
-        return new RtextConfigurationBuilder();
-    }
+	public static RtextConfigurationBuilder newBuilder() {
+		return new RtextConfigurationBuilder();
+	}
 
-    @SuppressWarnings("unchecked")
-    public RtextConfigurationBuilder registerTypeAdapter(Type type, IRtextTypeAdapter<?> typeAdapter) {
-        rtextConfiguration.typeAdapterFactories.add((IRtextTypeAdapterFactory) TypeAdaptersHelper.newTypeFactory(TypeToken.of(String.class), TypeToken.of(type), (IRtextTypeAdapter) typeAdapter));
-        return this;
-    }
+	public RtextConfigurationBuilder registerTypeAdapter(Type type, IRtextTypeAdapter<?> typeAdapter) {
+		rtextConfiguration.typeAdapterFactories.add(new RtextTypeAdapterFactoryAdapter(TypeAdaptersHelper
+				                                                                               .newTypeFactory(TypeToken
+						                                                                                               .of(String.class),
+				                                                                                               TypeToken
+						                                                                                               .of(type),
+				                                                                                               (IRtextTypeAdapter) typeAdapter)));
+		return this;
+	}
 
-    @SuppressWarnings("unchecked")
-    public RtextConfigurationBuilder registerTypeHierarchyAdapter(Class<?> type, IRtextTypeAdapter<?> typeAdapter) {
-        rtextConfiguration.typeAdapterFactories.add((IRtextTypeAdapterFactory) TypeAdaptersHelper.newTypeHierarchyFactory(String.class, type, (IRtextTypeAdapter) typeAdapter));
-        return this;
-    }
+	public RtextConfigurationBuilder registerTypeHierarchyAdapter(Class<?> type, IRtextTypeAdapter<?> typeAdapter) {
+		rtextConfiguration.typeAdapterFactories.add(new RtextTypeAdapterFactoryAdapter(TypeAdaptersHelper
+				                                                                               .newTypeHierarchyFactory(
+						                                                                               String.class,
+						                                                                               type,
+						                                                                               (IRtextTypeAdapter) typeAdapter)));
+		return this;
+	}
 
-    public RtextConfigurationBuilder registerTypeAdapterFactory(IRtextTypeAdapterFactory... typeAdapterFactories) {
-        rtextConfiguration.typeAdapterFactories.addAll(Arrays.asList(typeAdapterFactories));
-        return this;
-    }
+	public RtextConfigurationBuilder registerTypeAdapterFactory(IRtextTypeAdapterFactory... typeAdapterFactories) {
+		rtextConfiguration.typeAdapterFactories.addAll(Arrays.asList(typeAdapterFactories));
+		return this;
+	}
 
-    public IRtextConfiguration build() {
-        return rtextConfiguration;
-    }
+	public IRtextConfiguration build() {
+		return rtextConfiguration;
+	}
 
-    private static class RtextConfigurationImpl implements IRtextConfiguration {
+	private static class RtextConfigurationImpl implements IRtextConfiguration {
 
-        private final List<IRtextTypeAdapterFactory> typeAdapterFactories = new ArrayList<>();
+		private final List<IRtextTypeAdapterFactory> typeAdapterFactories = new ArrayList<>();
 
-        private RtextConfigurationImpl() {
-            super();
-        }
+		private RtextConfigurationImpl() {
+			super();
+		}
 
-        @Override
-        public List<IRtextTypeAdapterFactory> getTypeAdapterFactories() {
-            return ImmutableList.copyOf(typeAdapterFactories);
-        }
-    }
+		@Override
+		public List<IRtextTypeAdapterFactory> getTypeAdapterFactories() {
+			return ImmutableList.copyOf(typeAdapterFactories);
+		}
+	}
+
+	private static class RtextTypeAdapterFactoryAdapter implements IRtextTypeAdapterFactory {
+
+		private ITypeAdapterFactory<Rtext> typeAdapterFactory;
+
+		public RtextTypeAdapterFactoryAdapter(
+				ITypeAdapterFactory<Rtext> typeAdapterFactory) {
+			super();
+
+			this.typeAdapterFactory = typeAdapterFactory;
+		}
+
+		@Override
+		public <T> IRtextTypeAdapter<T> create(Rtext rtext, TypeToken<T> typeToken) {
+			return new RtextTypeAdapter<T>(typeAdapterFactory.create(rtext, TypeToken.of(String.class),
+			                                                         typeToken));
+		}
+	}
+
+	private static class RtextTypeAdapter<T> implements IRtextTypeAdapter<T> {
+
+		private ITypeAdapter<String, T> typeAdapter;
+
+		public RtextTypeAdapter(ITypeAdapter<String, T> typeAdapter) {
+			this.typeAdapter = typeAdapter;
+		}
+
+		@Override
+		public T toDst(String s) {
+			return typeAdapter.toDst(s);
+		}
+	}
 }
---
+++ src/test/java/com/talanlabs/rtext/test/it/RegistryIT.java
@@ -0,0 +1,48 @@
+package com.talanlabs.rtext.test.it;
+
+import com.google.common.base.Splitter;
+import com.google.common.reflect.TypeToken;
+import com.talanlabs.rtext.IRtextTypeAdapter;
+import com.talanlabs.rtext.Rtext;
+import com.talanlabs.rtext.configuration.RtextConfigurationBuilder;
+import org.assertj.core.api.Assertions;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class RegistryIT {
+
+	private static Rtext rtext;
+
+	@BeforeClass
+	public static void init() {
+		RtextConfigurationBuilder builder =
+				RtextConfigurationBuilder.newBuilder().registerTypeAdapter(new TypeToken<Map<String, String>>() {
+				}.getType(), new MapRtextTypeAdapter());
+
+		rtext = new Rtext(builder.build());
+	}
+
+	@Test
+	public void testMapRtext() {
+		Assertions.assertThat((Map<String, String>) rtext.fromText("toto:123", new TypeToken<Map<String, String>>() {
+		}.getType())).hasSize(1).containsEntry("toto", "123");
+		Assertions.assertThat(
+				(Map<String, String>) rtext.fromText("toto:123,tata:abc", new TypeToken<Map<String, String>>() {
+				}.getType())).hasSize(2).containsEntry("toto", "123").containsEntry("tata", "abc");
+	}
+
+	private static class MapRtextTypeAdapter implements IRtextTypeAdapter<Map<String, String>> {
+
+		@Override
+		public Map<String, String> toDst(String src) {
+			return Splitter.on(",").omitEmptyStrings().splitToList(src)
+					.stream()
+					.collect(Collectors.toMap(s -> s.substring(0, s.indexOf(":")), s -> s
+							.substring(s.indexOf(":") + 1)));
+		}
+
+	}
+}
